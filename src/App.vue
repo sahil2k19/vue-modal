9a7c9a57fe3f0ff8364a1bb7eeca842b5d8a2ea2
sahil2k19 <sahilgagan227@gmail.com>
2023-02-26 21:28:44 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <Modal/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue"
 
 export default {
+  
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Modal},
+  data(){
+    return {
+      title:"My First Vue App",
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name);
+      this.$refs.name.focus();
+      this.$refs.name.classList.add('sahil');
+     
+    }
   }
 }
 </script>
@@ -23,4 +39,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1 {
+  border-bottom:1px solid #ddd;
+  display: inline-block;
+  padding :10px;
+}
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal content</h1>
+
+            <p>Modal content</p>
+        </div> 
+    </div>
+</template>
+
+
+<script>
+export default{
+    components:[],             
+    props:[],
+    data(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+
+<style >
+    .backdrop{
+        top:0;
+        position:fixed;
+        background: rgba(0, 0, 0, 0.589);
+        width: 100%;
+        height:100%;
+    }
+    .modal{
+        width:400px;
+        padding:20px;
+        margin: 129px auto;
+        background : white;
+        border-radius: 10px ;
+    }
+
+    .modal h1{
+        color:aqua;
+        border:none;
+        padding:0;
+    }
+</style>
